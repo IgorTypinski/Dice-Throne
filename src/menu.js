6a4447bec88a0aa5189719b9d3b21e392bbc3bf6
Igor Typiński <251237@edu.p.ldoz.pl>
2026-04-12 22:49:31 +0200

    first commit

---
+++ src/menu.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("#btn");
+button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
+    alert("Klik działa 🚀");
+});
+export {};
+//# sourceMappingURL=menu.js.map
